d630e0c2917863702cfd59ab80b4bb495fc409ec
Chen Huang <chen1huang2@gmail.com>
2025-02-11 02:21:30 +0000

    Update experimental_condition_template.jl

--- src/script_templates/experimental_condition_template.jl
+++ src/script_templates/experimental_condition_template.jl
@@ -12,14 +12,14 @@ D = 84 #40cm camera length
 Δk = 2α / D
 θ = N * Δk
 Δx, Δy = uconvert.(Å, λ./θ)
-Δx * 128
+Δx * N[1]
 rₚ = probe_radius(α, Δf)
-Δx * 128 > rₚ * 1.3
+Δx * N[1] > rₚ * 1.3
 sₚ = probe_area(α, Δf)
 overlap, overlap_ratio = probe_overlap(rₚ, dₛ; ratio=true)
 
 function dose(i, t, dₛ)
-	uconvert(u"Å^-2", i * t / 1.62e-19u"C" / dₛ^2)
+    uconvert(u"Å^-2", i * t / 1.62e-19u"C" / dₛ^2)
 end
 
 i = 1.0pA
